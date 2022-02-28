95fb97729e29f72f188c4e2d99ce7c0c7903e8fd
AuroraPerego <au99.perego@gmail.com>
2022-03-01 00:54:05 +0100

    derivative trigger

--- SingleIRsource/instruments/PXIe-5170R_2.py
+++ SingleIRsource/instruments/PXIe-5170R_2.py
@@ -4,40 +4,112 @@
 # niscope module examples: https://nimi-python.readthedocs.io/en/master/niscope/examples.html
 # https://manualzz.com/doc/6830056/ni-scope-software-user-manual
 
-from operator import index
-import niscope
-import argparse
-import sys
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+import niscope as ni
+import h5py 
 from scipy.signal import savgol_filter
 
 class PXIeSignalAcq(object):
-
-    def __init__(self, device_address, trigger=0, channels=[0], records=3, sample_rate=5e7, length=4000):
+    def __init__(self, device_address, trigger: dict, channels=[0,1], records=3, sample_rate=5e7, length=4000):
         try:
-            self.session = niscope.Session(device_address)
+            self.session = ni.Session(device_address)
             print('Connected')
         except:
             print('Not connected')
 
-        self.length       = length
-        self.trigger      = trigger
-        self.channels     = channels
-        self.records      = records
-        self.record_index = 0
+        self.waveform, self.i_matrix, self.q_matrix = [], [], []
 
-        self.session.configure_vertical(range = 5, coupling=niscope.VerticalCoupling.DC)
+        self.length     = length
+        self.trigger    = trigger
+        self.channels   = channels
+        self.records    = records
+
+        self.session.configure_vertical(range=5, coupling=ni.VerticalCoupling.DC)
         self.session.configure_horizontal_timing(min_sample_rate=sample_rate, min_num_pts=length, ref_position=40.0, num_records=records, enforce_realtime=True)
 
-        self.session.trigger_type       = niscope.TriggerType.EDGE
-        self.session.trigger_source     = "0"
-        self.session.trigger_slope      = niscope.TriggerSlope.POSITIVE
-        self.session.trigger_level      = 0.5
-        self.session.trigger_delay_time = 0.0
+        self.session.trigger_type       = getattr(ni.TriggerType, trigger["trigger_type"])
+        self.session.trigger_source     = trigger["trigger_source"]
+        self.session.trigger_slope      = getattr(ni.TriggerSlope, trigger["trigger_slope"])
+        self.session.trigger_level      = trigger["trigger_level"]
+        self.session.trigger_delay_time = trigger["trigger_delay"]
 
         self.session.initiate()
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *exc):
+        self.session.close()
+
+    def derivative_trigger(self, index, n, limit):
+        # We can focus only one signal (I in this case) and then apply the corrections on both
+        sample = self.waveform[0][index].samples
+
+        first_derivative = np.diff(sample, n = 1)
+        n_points = 0
+        i = 0
+        max = np.std(sample[0:500]) #al massimo si può fare due sigma, il 500 va valutato in base a lenght e a ref_position
+        while(n_points<15): #si può aumentare n_points
+            n_points = n_points + 1 if first_derivative[i] > max else 0
+            i += 1
+        start = i - 15
+
+        begin = start - 25
+        end = start + 25    
+
+        derivative_func = savgol_filter(sample[begin:end], len(sample[begin:end])-1, 12, n, delta=1)
+
+        #limit NON va passato, lo decidiamo qui 
+        if (derivative_func.max() > limit):
+            # We need to understand and automatize the centering of impulse and resizing the window length
+            begin = start - 200 #quanti punti prima dell'inizio?
+            end = start + 2000 #quanto è lungo l'impulso?
+            self.waveform[0][index].samples = self.waveform[0][index].samples[begin:end]
+            self.waveform[1][index].samples = self.waveform[1][index].samples[begin:end]
+            return True
+        else:
+            return False
+
+    def read(self):
+        self.waveform.extend([self.session.channels[i].read(num_samples=self.length, timeout=0) for i in self.channels])
+        return None
+
+    def fill_matrix(self):
+        for i in range(self.records):
+            if (self.derivative_trigger(2, i)):
+                self.i_matrix.append(self.waveform[0][i].samples)
+                self.q_matrix.append(self.waveform[1][i].samples)
+        return None
+    
+    def storage_hdf5(self, name):
+        with h5py.File(name, 'w') as hdf:
+            hdf.create_dataset('i_signal', data=self.i_matrix, compression='gzip', compression_opts=9)
+            hdf.create_dataset('q_signal', data=self.q_matrix, compression='gzip', compression_opts=9)
+        return None
+
+
+trigger = dict(
+    trigger_type = 'EDGE', #or 'IMMEDIATE', 'DIGITAL'
+    trigger_source = '0',
+    trigger_slope = 'POSITIVE', # or 'NEGATIVE'
+    trigger_level = '0.5',
+    trigger_delay = '0.0'
+)
+with PXIeSignalAcq("PXI1Slot2", trigger) as test:
+    test = PXIeSignalAcq("PXI1Slot2", trigger)
+    test.read()
+    test.fill_matrix()
+    test.storage_hdf5('signals.h5')
+
+
+
+##########################
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class PXIeSignalAcq(object):
 
     def derivative_trigger(self, n, arr):
         # arr = self.waveform[0][self.record_index].samples
@@ -60,42 +132,6 @@ class PXIeSignalAcq(object):
         self.record_index += 1
 
         return arr[start:end]
-
-
-    def read(self):
-        wf = [] 
-        wf.extend([self.session.channels[i].read(num_samples=self.length, timeout=0) for i in self.channels])
-        self.waveform = wf
-        #print(wf)
-        return None
-
-    def create_dataframe(self):
-        index = []
-        index.extend([i for i in range(0, len(self.channels) * self.records)])
-        self.dataframe = pd.DataFrame(index = index, columns = ["Record", "Channel", "Samples"])
-        return None
-
-    def fill_dataframe(self):
-        for i in range(self.records):
-            check = self.derivative_trigger(2, self.waveform[0][i].samples)
-            if check == -1:
-                continue
-            else:
-                for j in self.channels:
-                    self.dataframe.loc[i*len(self.channels) + j]["Record"]  = i
-                    self.dataframe.loc[i*len(self.channels) + j]["Channel"] = j
-                    self.dataframe.loc[i*len(self.channels) + j]["Samples"] = list(self.waveform[j][i].samples)
-        # print(self.dataframe.loc[0]["Samples"])
-        return None
-
-    def save_dataframe(self, name = "data1.json"):
-        self.dataframe.to_json(name)
-        return None
-
-    def read_dataframe(self, name = "data1.json"):
-        prova = pd.read_json(name)
-        print(prova)
-        return None
     
     def plot_waveform(self):
         for i in range(self.records):
@@ -106,22 +142,6 @@ class PXIeSignalAcq(object):
             plt.scatter(x, sample)
             plt.savefig("imm" + str(i) + ".png")
         
-    # Set records (sampling frequency, rate, length, width,...)
-    # Set triggers (edge, immediate, digital, negative, positive,...)
-    # Set channels (addressing each channel)
-    def __enter__(self):
-        return self
-    def __exit__(self, *exc):
-        self.session.close()
-
-with PXIeSignalAcq("PXI1Slot2") as test:
-    #test = PXIeSignalAcq("PXI1Slot2")
-    test.read()
-    test.create_dataframe()
-    test.fill_dataframe()
-    test.save_dataframe()
-    test.read_dataframe()
-
 
 """
 def example(resource_name, channels, options, length, voltage):
