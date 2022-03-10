50edd3597e5a591d7c2dff415b2edecc28dcda0f
AuroraPerego <au99.perego@gmail.com>
2022-03-11 00:55:36 +0100

    find minimum w/ parabola

--- SingleIRsource/simulated/trigger.py
+++ SingleIRsource/simulated/trigger.py
@@ -19,30 +19,20 @@ def gen_signal(x=np.linspace(0,1000,1000), baseline=0, amplitude=140, rise_time=
 def gen_noise(x=np.linspace(0,1000,1000), noise_lev=0.5):
     return np.random.normal(scale=noise_lev, size=len(x))
 
-def derivative_trigger(sample, avg, n=2, plot=False):
+def derivative_trigger(sample, window_ma, n=2, plot=False):
     i = 0
     # Initialize an empty list to store moving averages
-    moving_averages = []
-    window_size = avg
-    
-    # Loop through the array to consider
-    # every window of size 3
-    while i < len(sample) - window_size + 1:
-        
-        # Store elements from i to i+window_size
-        # in list to get the current window
-        window = sample[i : i + window_size]
-    
-        # Calculate the average of current window
-        window_average = round(sum(window) / window_size, 2)
-        
-        # Store the average of current
-        # window in moving average list
-        moving_averages.append(window_average)
-        
-        # Shift window to right by one position
-        i += 1
-        
+    weights = np.full(window_ma, 1/window_ma)
+    moving_averages = convolve(sample, weights, mode='mirror')
+
+    time = np.linspace(0,len(sample), len(sample))
+
+    # Plot after moving average 
+    if plot:
+        plt.plot(time, moving_averages)
+        print('Sample')
+        plt.show()
+
     first_derivative = np.gradient(moving_averages)
     std = np.std(first_derivative[0:100])/2 #100 will become a function of length and pos_ref in pxie
     index_min = first_derivative.argmin()
@@ -54,17 +44,7 @@ def derivative_trigger(sample, avg, n=2, plot=False):
         rise_points += 1
     
     #print('rise_points = ', rise_points)
-
-    ######
-    #this part of the code could be used to check whether it's signal or noise
-    #check is less than 10 for noise and more than 100 for signal (e.g. for cutoff: len(sample)/10)
-    #check = 0
-    #for i in range(len(sample)):
-    #    check = check + 1 if (sample[i] < np.mean(sample[0:100])- 3*np.std(sample[0:100])) else check
-    #
-    #print(check)
-    ######
-
+    
     a = 10 #to have a window_length of 21, in this way all the windows are equal
     b = a // 2
     start = index_min - rise_points
@@ -99,68 +79,22 @@ def derivative_trigger(sample, avg, n=2, plot=False):
         plt.grid()
         plt.show()
 
-    return begin+b+(derivative_func[b:-b].argmin())#, derivative_func[b+derivative_func[b:-b].argmin()]
-    #return print('start: ', time[begin+b+derivative_func[b:-b].argmin()])
-
-def derivative_trigger_avg(sample, avg, n=2, plot=False):
-    moving_averages = avg
-    first_derivative = np.gradient(moving_averages)
-    std = np.std(first_derivative[0:100])/2 #100 will become a function of length and pos_ref in pxie
-    index_min = first_derivative.argmin()
-
-    #print('index_min = ', index_min)
-    
-    rise_points = 0
-    while first_derivative[index_min - rise_points] < -std:
-        rise_points += 1
-    
-    #print('rise_points = ', rise_points)
-
-    ######
-    #this part of the code could be used to check whether it's signal or noise
-    #check is less than 10 for noise and more than 100 for signal (e.g. for cutoff: len(sample)/10)
-    #check = 0
-    #for i in range(len(sample)):
-    #    check = check + 1 if (sample[i] < np.mean(sample[0:100])- 3*np.std(sample[0:100])) else check
-    #
-    #print(check)
-    ######
-
-    a = 10 #to have a window_length of 21, in this way all the windows are equal
-    b = a // 2
-    start = index_min - rise_points
-
-    if start < a:
-        start = a
-    if start > len(sample)-a:
-        start = len(sample)-1-a
-    
-    end = start + a + 1     # +1 to avoid the error: "window_length must be odd."
-    begin = start - a if start - a > 0 else 0 # To avoid negative values for begin
-    
-    #print('hint start = %d, begin = %d, end = %d' %(start, begin, end))
-    
-    window_length = len(sample[begin:end]) #-1 if len(sample[begin:end]) % 2 == 0 else len(sample[begin:end])
-
-    #print(window_length)
-
-    #poly_order = window_length-1 if window_length < 14 else 12
-    
-    derivative_func = savgol_filter(sample[begin:end], window_length, 8, n, delta=1) #8 is the best in the tests done
-
-    # we have to drop the first b points and the last b points of the array
-    # since sth strange happens here with the derivative due to the polinomial fitting of sav_gol
-    time = np.linspace(0,len(sample), len(sample))
-
+    x2 = begin+b+(derivative_func[b:-b].argmin())
+    y1 = derivative_func[b+derivative_func[b:-b].argmin() - 1]
+    y2 = derivative_func[b+derivative_func[b:-b].argmin()]
+    y3 = derivative_func[b+derivative_func[b:-b].argmin() + 1]
+    min = vertex_parabola(x2, y1, y2, y3)
+    aa, bb, cc = coeff_parabola(x2, y1, y2, y3)
     if plot:
-        plt.scatter(time[begin:end], savgol_filter(sample[begin:end], window_length, 8, 0, delta=1), color='dodgerblue')
-        plt.scatter(time[begin+b:end-b], derivative_func[b:-b], color="g")
+        t = time[begin:end]
+        plt.scatter(t, aa*t*t + bb*t +cc, color='dodgerblue')
         plt.xlabel('Time [$\mu$s]')
         plt.ylabel('Voltage [mV]')
         plt.grid()
         plt.show()
+    #min = begin+b+(derivative_func[b:-b].argmin())
 
-    return begin+b+(derivative_func[b:-b].argmin())#, derivative_func[b+derivative_func[b:-b].argmin()]
+    return min, x2#, derivative_func[b+derivative_func[b:-b].argmin()]
     #return print('start: ', time[begin+b+derivative_func[b:-b].argmin()])
 
 def efficiency(index, x=np.linspace(0,1000,1000), pulse_start=200):
@@ -175,8 +109,7 @@ def efficiency(index, x=np.linspace(0,1000,1000), pulse_start=200):
 
     return d/d_max
 
-# Si tratta solo di un calcolo su matrici anziché su vettori, il ragionamento è lo stesso precedente
-# ma pensato in ottimizzazione delle risorse
+#to optimize the performance
 def efficiency_matrix(indexes, x=np.linspace(0,1000,1000), pulse_start=200):
     normalized_distances = []
 
@@ -199,18 +132,28 @@ def get_efficiency(eff, n_sample):
 def vertex_parabola(x2, y1, y2, y3):
     x1 = x2 - 1
     x3 = x2 + 1
-    b = x3*x3*(y2-y1) + x2*x2*(y3-y1) + x1*x1*(y3-y2)
-    a = (y2-y3)*x1 + (y1-y3)*x2 + (y1-y2)*x3
-    den = (x1-x2)*(x1-x3)*(x3-x2)
+    b = x3 * x3 * (y2 - y1) + x2 * x2 * (y1 - y3) + x1 * x1 * (y3 - y2)
+    a = (y2 - y3) * x1 + (y3 - y1) * x2 + (y1 - y2) * x3
+    #den = (x1-x2)*(x1-x3)*(x3-x2)
     return -b/(2*a)
 
-# Moving average basato sul cumsum
+def coeff_parabola(x2, y1, y2, y3):
+    x1 = x2 - 1
+    x3 = x2 + 1
+    den = (x1 - x2) * (x1 - x3) * (x3 - x2)
+    b = (x3 * x3 * (y2 - y1) + x2 * x2 * (y1 - y3) + x1 * x1 * (y3 - y2)) / den
+    a = ((y2 - y3) * x1 + (y3 - y1) * x2 + (y1 - y2) * x3) / den
+    c = ((x2 * x3 * y1 * (x3 - x2) + x3 * x1* y2  * (x1 - x3) + x1 * x2 * y3 * (x2 - x1))) / den
+
+    return a, b, c
+
+# Moving average based on cumsum
 def moving_average(matrix, window_ma) :
     ret = np.cumsum(matrix, axis=1, dtype=float)
     ret[:, window_ma:] = ret[:, window_ma:] - ret[:, :-window_ma]
     return ret[:, window_ma - 1:]
 
-def derivative_trigger_matrix(sample, window_ma, n=2, mv='convolve', plot=False): #now sample is a matrix of all of the samples
+def derivative_trigger_matrix(sample, window_ma, n=2, mv='convolve', vertex = True, plot=False): #now sample is a matrix of all of the samples
     
     if mv == 'convolve':
         weights = np.full((1, window_ma), 1/window_ma)
@@ -223,7 +166,7 @@ def derivative_trigger_matrix(sample, window_ma, n=2, mv='convolve', plot=False)
     for ii in range(len(sample)):
         time = np.linspace(0,len(sample[ii]), len(sample[ii]))
 
-        # Plot dei grafici dopo aver generato la moving average sia che sia cumsum che convolve
+        # Plot after moving average 
         if plot:
             plt.plot(time, moving_averages[ii])
             print('Sample: ', ii)
@@ -269,26 +212,41 @@ def derivative_trigger_matrix(sample, window_ma, n=2, mv='convolve', plot=False)
             plt.ylabel('Voltage [mV]')
             plt.grid()
             plt.show()'''
+
         x2 = begin+b+(derivative_func[b:-b].argmin())
-        y1 = derivative_func[b+derivative_func[b:-b].argmin() - 1]
-        y2 = derivative_func[b+derivative_func[b:-b].argmin()]
-        y3 = derivative_func[b+derivative_func[b:-b].argmin() + 1]
-        # min = vertex_parabola(x2, y1, y2, y3)
-        min = begin+b+(derivative_func[b:-b].argmin())
+        if vertex:
+            y1 = derivative_func[b+derivative_func[b:-b].argmin() - 1]
+            y2 = derivative_func[b+derivative_func[b:-b].argmin()]
+            y3 = derivative_func[b+derivative_func[b:-b].argmin() + 1]
+            min = vertex_parabola(x2, y1, y2, y3)
+        else:
+            min = x2
+        #min = begin+b+(derivative_func[b:-b].argmin())
         # we have to drop the first b points and the last b points of the array
         # since sth strange happens here with the derivative due to the polinomial fitting of sav_gol
-        
-        # Ho commentato il minimo precedente della parabola che non torna, dai tu un'occhiata,
-        # al momento lavoro con il vecchio così da avere un riferimento se tutto funziona sull'efficienza
         index_mins.append(min)
 
     return index_mins
 
-def good_plot(x, y, title = 'title', x_label = 'x', y_label = 'y'):
-    fig = plt.figure(dpi = 300)
-    fig.patch.set_facecolor('white')       
+def good_plot(x, y, title = 'title', x_label = 'x', y_label = 'y', good = False):
+    if good:
+        fig = plt.figure(dpi = 300)
+        fig.patch.set_facecolor('white')       
     plt.plot(x, y)
     plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.grid()
+    plt.show()
+
+def multiple_plots(x, *args, title = 'title', x_label = 'x', y_label = 'y', good = True):
+    if good:
+        fig = plt.figure(dpi = 300)
+        fig.patch.set_facecolor('white')     
+    for i in range(0, len(args), 2):  
+        plt.plot(x, args[i], label=args[i+1])
+    plt.title(title)
+    plt.legend()
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.grid()
